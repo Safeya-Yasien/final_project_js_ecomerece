6a1bad4344d5e57d597a3db6ece5afd895a553c8
Safeya-Yasien <ssafeya5@gmail.com>
2026-01-22 14:39:59 +0200

    feat: add page protection - Implemented login check on protected pages - Users and admins are redirected to login.html if not authenticated - Created reusable auth.js module to avoid repeating checks on multiple pages

--- admin/js/auth.js
+++ admin/js/auth.js
@@ -1,11 +1,16 @@
+const isLoggedIn = localStorage.getItem("isLoggedIn");
+if (!isLoggedIn) {
+  window.location.href = "../login.html";
+}
+
 const logoutBtn = document.querySelector(".logout-btn");
 if (logoutBtn) {
   logoutBtn.addEventListener("click", () => {
     const confirmMessage = confirm("Are you sure you want to logout?");
     if (confirmMessage) {
-      // localStorage.clear();
-      // window.location.reload();
-      console.log("logout");
+      localStorage.removeItem("isLoggedIn");
+      localStorage.removeItem("user");
+      window.location.href = "../login.html";
     }
   });
 }
